db8fec1621eeb272530c738914c5248a17f8b8e9
Szebedy, Bence <bence.szebedy@albis-engineering.com>
2021-11-01 17:49:44 +0100

    Updated code for new website

--- alifilter.js
+++ alifilter.js
@@ -7,19 +7,19 @@ document.addEventListener('DOMContentLoaded', function() {
 			chrome.tabs.executeScript(tabs[0].id,{code: `
 				var interval = setInterval(function(){
 					var minrating = ` + minrating + `
-					var ratings = document.getElementsByClassName('_1hEhM');
+					var ratings = document.getElementsByClassName('eXPaM');
 					var badratings = Array.prototype.filter.call(ratings, function(rating){
 						return rating.innerHTML < minrating;
 					});
 					for(var i = 0; i < badratings.length; i++){
-						var j = 10;
+						var j = 2;
 						var el = badratings[i].parentElement;
-						while(el.className != '_1OUGS'){
+						//while(el.className != '_2f4Ho _3t7zg'){
+						while(j-- > 0){
 							el = el.parentElement;
-							j--;
-							if(j == 0) break;
 						}
-						if(j != 0) el.style.display = 'none'; // depending on what you're doing
+						//if(j != 0) el.style.display = 'none'; // depending on what you're doing
+						el.style.display = 'none';
 					}
 				}, 300);
 			`});
