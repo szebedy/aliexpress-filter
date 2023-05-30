c98b27de1495571fd7dc9b84f0b50b2d2b6211d8
Bence Szebedy <bence.szebedy@albis-engineering.com>
2023-05-30 14:01:06 +0200

    Updated to manifest V3

--- alifilter.js
+++ alifilter.js
@@ -4,23 +4,11 @@ document.addEventListener('DOMContentLoaded', function() {
 	filterButton.addEventListener('click', function() {
 		var minrating = document.getElementById('numberInput').value;
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			chrome.tabs.executeScript(tabs[0].id,{code: `
-				var interval = setInterval(function(){
-					var minrating = ` + minrating + `
-					var ratings = document.getElementsByTagName('span');
-					var badratings = Array.prototype.filter.call(ratings, function(rating){
-						return /^([1-4]\.[0-9])$/.test(rating.innerHTML) && rating.innerHTML < minrating;
-					});
-					for(var i = 0; i < badratings.length; i++){
-						var j = 2;
-						var el = badratings[i].parentElement;
-						while(j-- > 0){
-							el = el.parentElement;
-						}
-						el.style.display = 'none';
-					}
-				}, 300);
-			`});
+			chrome.scripting.executeScript({
+				target: {tabId: tabs[0].id},
+				func: filter,
+				args : [ minrating ],
+			});
 		});
 
 		
@@ -31,6 +19,23 @@ document.addEventListener('DOMContentLoaded', function() {
 		updateNumberInput(4 + 0.1*document.getElementById('rangeInput').value);
 	};
 }, false);
+
+function filter(minrating) {
+	var interval = setInterval(function(){
+	var ratings = document.getElementsByTagName('span');
+	var badratings = Array.prototype.filter.call(ratings, function(rating){
+		return /^([1-4]\.[0-9])$/.test(rating.innerHTML) && rating.innerHTML < minrating;
+	});
+	for(var i = 0; i < badratings.length; i++){
+		var j = 2;
+		var el = badratings[i].parentElement;
+		while(j-- > 0){
+			el = el.parentElement;
+		}
+		el.style.display = 'none';
+	}
+}, 300);
+}
 
 function updateNumberInput(val) {
   document.getElementById('numberInput').value=val;
